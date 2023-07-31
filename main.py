38a6d7b7bca2994685f4fb3a3764a1e6e3e9b10a
DanielVargas <danielv464@gmail.com>
2023-07-31 02:18:17 -0400

    Cerrando los commits de hoy

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from mtcnn.mtcnn import MTCNN
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
 
 
+def draw_facebox(filename, result_list):
+  # load the image
+  data = plt.imread(filename)
+  # plot the image
+  plt.imshow(data)
+  # get the context for drawing boxes
+  ax = plt.gca()
+  # plot each box
+  for result in result_list:
+    # get coordinates
+    x, y, width, height = result['box']
+    # create the shape
+    rect = plt.Rectangle((x, y), width, height, fill=False, color='green')
+    # draw the box
+    ax.add_patch(rect)
+    # show the plot
+  plt.show()# filename = 'test1.jpg' # filename is defined above, otherwise uncomment
+  # load image from file
+
+
+filename = "./images/familia.jpg" #File location
+pixels = plt.imread(filename) # defined above, otherwise uncomment
+# detector is defined above, otherwise uncomment
+detector = mtcnn.MTCNN()
+# detect faces in the image
+faces = detector.detect_faces(pixels)
+# display faces on the original image
+draw_facebox(filename, faces)
